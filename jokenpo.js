8ea58cf660afc028776190bf68a4451c9913b354
lucenajj <lucenajj@gmail.com>
2019-08-22 18:43:53 -0300

    Arquivo jokenpojsjs deletado

---
+++ jokenpo.js
@@ -0,0 +1,14 @@
+function jogar() {
+    if (document.getElementById("pedra").che == false && 
+    document.getElementById("papel").spellcheck == false && 
+    document.getElementById("tesoura").spellcheck  == false) {
+        alert("Escolha uma das opções")
+        
+    }else{
+        
+    }
+}
+
+function resetar() {
+    
+}
